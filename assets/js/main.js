f13be72f43e835017d7283b56d44fb79679fb3c9
Obi Zim <52291700+Obizim@users.noreply.github.com>
2020-07-21 21:36:36 -0700

    Gsap

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+// HorizontalScroll
+
 var blocks = document.getElementsByClassName("block");
 var container = document.getElementsByClassName("container");
 var hs = new HorizontalScroll.default({
@@ -5,4 +7,40 @@ var hs = new HorizontalScroll.default({
     container: container,
     isAnimated: true,
     springEffect: 0.8,
+    skewReducer: 35
 });
+
+// AnimationEffect
+
+const tl = gsap.timeline();
+tl
+    .from('.brand-logo', {
+        xPercent: -100,
+        opacity: 0,
+    })
+    .from('nav h1', {
+        yPercent: -100,
+        opacity: 0
+    })
+    .from('.home_heading, .home_paragraph, .btn', {
+        xPercent: -20,
+        opacity: 0,
+        stagger: 0.2,
+
+    })
+    .from('.home img', {
+        opacity: 0,
+        stagger: 0.2,
+        duration: 1
+    })
+    .from('.cat_text .heading1, .heading2', {
+        xPercent: -20,
+        opacity: 0,
+        stagger: 0.2,
+    })
+    .from('.cat_text .paragraph', {
+        xPercent: -20,
+        opacity: 0,
+        stagger: 0.2,
+    })
+
